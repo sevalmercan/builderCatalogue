708619d0f30370284e38ecca2c69446565b33950
sevalmercan <sevalmercan.sm@gmail.com>
2023-03-04 20:23:41 +0300

    lego card moved to other dic

---
+++ src/components/legoSetCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="set-card-wrapper">
+        <div class="lego-set-card">
+
+            <img src="../assets/images/legoset.jpg" alt="Lego Set">
+
+            <div class="info">
+                <div>
+                    Name : alien-spaceship
+                </div>
+                <div>
+                    Set Number : 497XX
+                </div>
+                <div>
+                    Total Pieces: 1050
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+</template>
+  
+<style lang="scss" scoped>
+@import '../assets/style/color.scss';
+
+
+
+
+.set-card-wrapper {
+    color: $primar-text-color;
+
+    .lego-set-card {
+        width: 500px;
+        margin-right: 8px;
+        margin-bottom: 8px;
+        border-radius: 6px;
+        overflow: hidden;
+        color: $primar-text-color;
+        border: $button-bg solid;
+        height: 165px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+
+        img {
+            width: auto;
+            height: auto;
+            margin: 5px;
+            border-radius: 15px;
+        }
+
+        .info {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+        }
+
+    }
+
+    .lego-set-card>div {
+        display: flex;
+        justify-content: center;
+    }
+}
+</style>
--- src/views/LegoSetsView.vue
+++ src/views/LegoSetsView.vue
@@ -1,69 +1,38 @@
 <template>
-  <div class="lego-set-container">
-    <div class="set-card-wrapper">
-      <div class="lego-set-card">
+  <div class="lego-set">
+    <div class="lego-card-container">
 
-        <img src="../assets/images/legoset.jpg" alt="Lego Set">
-
-        <div class="info">
-          <div>
-            Name : alien-spaceship
-          </div>
-          <div>
-            Set Number : 497XX
-          </div>
-          <div>
-            Total Pieces: 1050
-          </div>
-        </div>
-
-      </div>
-
-
+      <lego-set-card />
+      <lego-set-card />
+      <lego-set-card />
+      <lego-set-card />
     </div>
 
   </div>
 </template>
 
+<script>
+
+import legoSetCard from '@/components/legoSetCard.vue';
+export default {
+
+  components: {
+    legoSetCard
+  }
+}
+</script>
+
 <style lang="scss" scoped>
 @import '../assets/style/color.scss';
 
-.lego-set-container {
-  color: $primar-text-color;
+.lego-set {
 
-  .set-card-wrapper {
 
-    .lego-set-card {
-      max-width: 300px;
-      margin-right: 8px;
-      margin-bottom: 8px;
-      border-radius: 6px;
-      overflow: hidden;
-      color: $primar-text-color;
-      border: $button-bg solid;
-      height: 105px;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-around;
-
-      img {
-        width: auto;
-        height: auto;
-        margin: 5px;
-        border-radius: 15px;
-      }
-
-      .info {
-        display: flex;
-        flex-direction: column;
-      }
-
-    }
-
-    .lego-set-card>div {
-      display: flex;
-      justify-content: center;
-    }
+  .lego-card-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 50%;
   }
 }
 </style>
