c786abd5ae62d8cfb3c8d91c680c5b8edfa8139b
sevalmercan <sevalmercan.sm@gmail.com>
2023-03-04 20:06:50 +0300

    lego set card implemented

--- src/views/LegoSetsView.vue
+++ src/views/LegoSetsView.vue
@@ -1,5 +1,69 @@
 <template>
-  <div class="about">
-    <h1>Lego Sets</h1>
+  <div class="lego-set-container">
+    <div class="set-card-wrapper">
+      <div class="lego-set-card">
+
+        <img src="../assets/images/legoset.jpg" alt="Lego Set">
+
+        <div class="info">
+          <div>
+            Name : alien-spaceship
+          </div>
+          <div>
+            Set Number : 497XX
+          </div>
+          <div>
+            Total Pieces: 1050
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import '../assets/style/color.scss';
+
+.lego-set-container {
+  color: $primar-text-color;
+
+  .set-card-wrapper {
+
+    .lego-set-card {
+      max-width: 300px;
+      margin-right: 8px;
+      margin-bottom: 8px;
+      border-radius: 6px;
+      overflow: hidden;
+      color: $primar-text-color;
+      border: $button-bg solid;
+      height: 105px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+
+      img {
+        width: auto;
+        height: auto;
+        margin: 5px;
+        border-radius: 15px;
+      }
+
+      .info {
+        display: flex;
+        flex-direction: column;
+      }
+
+    }
+
+    .lego-set-card>div {
+      display: flex;
+      justify-content: center;
+    }
+  }
+}
+</style>
