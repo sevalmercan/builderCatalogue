294509f10473c642ce06f929c8523d72492046eb
sevalmercan <sevalmercan.sm@gmail.com>
2023-03-08 00:35:26 +0300

    total info css changed

--- src/components/legoSetDetails.vue
+++ src/components/legoSetDetails.vue
@@ -4,15 +4,15 @@
             <div class="single-set">
                 <div class="total-info">
                     <div>
-                        Piece Id : {{ set.designID }}
+                        Piece no: {{ set.designID }}
                     </div>
 
                     <div>
-                        Total Brick : {{ calculateTotalBrick(set.variants) }}
+                        Total bricks: {{ calculateTotalBrick(set.variants) }}
                     </div>
                 </div>
-                <div v-for="pieceStatistic in set.variants" :key="pieceStatistic.color">
-                    <div class="piece-statistics">
+                <div class="single-brick">
+                    <div class="piece-statistics" v-for="pieceStatistic in set.variants" :key="pieceStatistic.color">
                         <div class="piece-result">
                             <div class="circle" :style="{ 'background': getColor(pieceStatistic.color) }">
                                 {{ pieceStatistic.count }}
@@ -72,44 +72,50 @@ export default {
     color: $primar-text-color;
     display: flex;
     flex-direction: column;
-    border-bottom: $button-bg solid;
 
     .single-set {
-        border-bottom: $button-bg solid;
+        border-bottom: $border-bg solid;
 
         .total-info {
             display: flex;
             justify-content: space-around;
-            margin: 18px 45px;
+            margin: 1.5rem;
         }
 
-        .piece-statistics {
+        .single-brick {
             display: flex;
             flex-direction: column;
-            position: relative;
+            row-gap: 1.1rem;
+            padding: 0 1.2rem;
+            margin-bottom: 1.5rem;
 
-            .piece-result {
+            .piece-statistics {
                 display: flex;
-                align-items: center;
-                justify-content: flex-start;
-                margin-left: 30px;
-                margin-bottom: 15px;
+                flex-direction: column;
+                position: relative;
 
-                .circle {
-                    width: 45px;
-                    height: 45px;
-                    -moz-border-radius: 50px;
-                    -webkit-border-radius: 50px;
-                    border-radius: 50px;
+                .piece-result {
                     display: flex;
-                    justify-content: center;
                     align-items: center;
-                    margin-right: 15px;
+                    justify-content: flex-start;
 
+                    .circle {
+                        width: 45px;
+                        height: 45px;
+                        -moz-border-radius: 50px;
+                        -webkit-border-radius: 50px;
+                        border-radius: 50px;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        margin-right: 15px;
+
+                    }
                 }
-            }
 
+            }
         }
+
     }
 
 
